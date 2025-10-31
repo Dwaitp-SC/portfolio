637170efeef497243d77731354e22061f8465a1c
Dwait Pandhi <129187222+Dwaitp-SC@users.noreply.github.com>
2025-10-31 15:40:14 +0000

    messed around a bit. commiting to do this at home.

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,13 +2,13 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Dillion Verma",
-  initials: "DV",
-  url: "https://dillion.io",
-  location: "San Francisco, CA",
-  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  name: "Dwait Pandhi",
+  initials: "DP",
+  url: "https://github.com/gamerwaves",
+  location: "Oakville, ON, Canada",
+  locationLink: "https://www.google.com/maps/place/oakville",
   description:
-    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+    "Student here. idk what to write here so im just typing random things. i started coding when i was 8 and i got inspired by steve jobs and how he changed the world with new technology.",
   summary:
     "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
   avatarUrl: "/me.png",
